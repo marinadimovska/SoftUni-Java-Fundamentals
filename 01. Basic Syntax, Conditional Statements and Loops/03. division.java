9d812cf23150823fa96d7d343308c11e80735e73
marinadimovska <110173709+marinadimovska@users.noreply.github.com>
2022-09-08 17:50:36 +0300

    Javascript Fundamentals Exercises

---
+++ 01. Basic Syntax, Conditional Statements and Loops/03. division.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        int num = 0;
+        boolean isDivisible = true;
+
+        if(number % 10 == 0){
+            num = 10;
+        }else if(number % 6 == 0){
+            num = 6;
+        }
+        else if(number % 3 == 0){
+            num = 3;
+        }else if(number % 2 == 0){
+            num = 2;
+        }else{
+            isDivisible = false;
+        }
+
+        if(isDivisible){
+            System.out.printf("The number is Divisible by %d", num);
+        }else{
+            System.out.println("Not divisible!");
+        }
+    }
+}
