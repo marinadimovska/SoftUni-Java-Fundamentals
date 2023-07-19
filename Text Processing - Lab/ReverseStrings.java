fb2e10c2b0813576b9515b8e550d2bb788f87e95
marinadimovska <mdimovska08@gmail.com>
2023-07-19 15:36:49 +0300

    SoftUni-Java-Fundamentals

---
+++ Text Processing - Lab/ReverseStrings.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        while(!text.equals("end")){
+            String reversedWord = "";
+            for (int i = text.length()-1; i >=0 ; i--) {
+                char symbol = text.charAt(i);
+
+                reversedWord +=symbol;
+            }
+            System.out.printf("%s = %s%n", text,reversedWord);
+
+
+            text = scanner.nextLine();
+        }
+    }
+}
